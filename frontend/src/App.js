5b2a0348feed6f55fe1dd82ab649884cac7d1265
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2023-12-13 17:28:56 -0600

    add frontend App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import WalletInfo from './components/WalletInfo';
+
+function App() {
+    return (
+        <div className="App">
+            <WalletInfo />
+        </div>
+    );
+}
+
+export default App;
